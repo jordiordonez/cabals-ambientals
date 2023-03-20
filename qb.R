8593798db1d66f5e26f0856062ef86aaf6d52199
Oriol Travesset <travesset@gmail.com>
2023-03-20 11:35:58 +0100

    Afegit qb.R
    Script inicial de càlcul del cabal base de manteniment (Qb)

---
+++ qb.R
@@ -0,0 +1,42 @@
+library(dplyr)
+library(lubridate)
+
+cabals<-read.csv("sample.csv", header=TRUE,sep=',')
+
+#recuperem els anys per obtenir valors diaris consecutius per anys 
+cabals<-mutate(cabals,any = year(as.Date(Fecha,"%d/%m/%Y")))
+
+#Eliminem la columna Fecha 
+cabals<-cabals[,-c(1)]
+
+#fem que Media siguin considerats com a numèrics després de substituir la , per .
+cabals<-mutate(cabals,Media=as.numeric(sub(",",".",cabals$Media)))
+
+#per cada j=1 a 100 Creo un resum amb mitjanes mòbils d'ordre j
+#per cada any i escullo els mínims per cada any per aquestes mitjanes d'ordre j 
+#calculo la mitjana en un vector vs
+Qba=NULL
+for (an in 2011:2020){
+  vr=NULL
+  vj=NULL  
+  vs=NULL
+  for (j in 1:100){
+    v=NULL
+    ultim=365-j+1
+    for (dia in 1:ultim){
+      diafi=dia+j-1
+      v[dia]=mean(cabals[cabals$any==an,]$Media[dia:diafi])}
+    vs[j]=min(v)}
+  for (j in 1:99){
+    if (vs[j]!=0){vr[j]=(vs[j+1]-vs[j])/vs[j]}}
+    
+  #calculem el màxim de les variacions relatives
+  m=max(vr[!is.na(vr)])
+  #recuperem el màxim dels valors de mitjanes pels quals hem trobat aquest màxim
+  posicio=which((vr==m))
+  Qba[an-2010]=vs[posicio+1]
+  }
+
+Qb=mean(Qba)
+  
+print(paste('El cabal base és',Qb,'m3/s'))
