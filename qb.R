8691520e23b6ccaa4184dcbe2bc99d3b23701cbb
jordiordonez <jordiordonez@hotmail.com>
2023-03-22 19:19:12 +0100

    Update qb.R
    Descric els valors d'anys com a Year_init i Year_final així com Max_order i reanomeno les variables
    Qba -> Qb_Year
    j->order
    v->means_order
    dia->Day
    ultim->End_day
    vs->min_values
    vr->ratio
    agrupo en el calcul del Qb_Year el calcul de m i de posicio
    suprimeixo comentaris ja que els noms de variables permeten la lectura

--- qb.R
+++ qb.R
@@ -2,9 +2,13 @@ library(dplyr)
 library(lubridate)
 
 file="sample.csv"
+Year_init=2011
+Year_final=2020
+Max_order=100
+
 cabals<-read.csv(file, header=TRUE,sep=',')
 
-#Recuperem els anys per obtenir valors diaris consecutius per anys 
+
 cabals<-cabals %>% 
   mutate(Year = year(as.Date(Fecha,"%d/%m/%Y")),
          Media=as.numeric(sub(",",".",cabals$Media))) %>%
@@ -12,31 +16,28 @@ cabals<-cabals %>%
 
 
 
-#Per cada j=1 a 100 Creo un resum amb mitjanes mòbils d'ordre j
-#Per cada any i escullo els mínims per cada any per aquestes mitjanes d'ordre j 
-#Calculo la mitjana en un vector vs
-Qba=NULL
-for (an in 2011:2020){
-  vr=NULL
-  vj=NULL  
-  vs=NULL
-  for (j in 1:100){
-    v=NULL
-    ultim=365-j+1
-    for (dia in 1:ultim){
-      diafi=dia+j-1
-      v[dia]=mean(cabals[cabals$any==an,]$Media[dia:diafi])}
-    vs[j]=min(v)}
-  for (j in 1:99){
-    if (vs[j]!=0){vr[j]=(vs[j+1]-vs[j])/vs[j]}}
+
+Qb_Year=NULL
+for (Year in Year_init:Year_final){
+  ratio=NULL
+  min_values=NULL  
+  for (order in 1:Max_order){
+    means_order=NULL
+    Last_day=sum(cabals$Year==Year)-order+1
+    for (Day in 1:Last_day){
+      End_day=Day+order-1
+      means_order[Day]=mean(cabals[cabals$Year==Year,]$Media[Day:End_day])}
+    min_values[order]=min(means_order)}
+  for (order in 1:(Max_order-1)){
+    if (min_values[order]!=0){
+      ratio[order]=(min_values[order+1]-min_values[order])/min_values[order]}}
     
-  #Calculem el màxim de les variacions relatives
-  m=max(vr[!is.na(vr)])
-  #Recuperem el màxim dels valors de mitjanes pels quals hem trobat aquest màxim
-  posicio=which((vr==m))
-  Qba[an-2010]=vs[posicio+1]
+
+
+  Qb_Year[Year-(Year_init)+1]=
+    min_values[which((ratio==max(ratio[!is.na(ratio)])))+1]
   }
 
-Qb=mean(Qba)
+Qb=mean(Qb_Year)
   
 print(paste('El cabal base és',Qb,'m3/s'))
