78d6d929c1dcb967c3c63371a7fa8b0e66381dbb
jordiordonez <jordiordonez@hotmail.com>
2023-03-23 05:47:49 +0100

    Update qb.R
    Aplicant lintr canvis estil noms snake_case

--- qb.R
+++ qb.R
@@ -2,60 +2,56 @@ library(dplyr)
 library(lubridate)
 
 file <- "sample.csv"
-Year_init <- 2011
-Year_final <- 2020
-Max_order <- 100
+year_init <- 2011
+year_final <- 2020
+max_order <- 100
 
 flows <- read.csv(file, header = TRUE, sep = ",")
-
 
 flows <- flows %>%
   mutate(Year = year(as.Date(Fecha, "%d/%m/%Y")),
          Media = as.numeric(sub(",", ".", flows$Media))) %>%
   select(Year, Media)
 
-
-
-min_value <- function(Year, order, year_days, flows) {
+min_value <- function(year, order, year_days, flows) {
   means_order <- NULL
-  Last_day <- year_days - order + 1
-  for (Day in 1:Last_day) {
-    End_day <- Day + order - 1
+  last_day <- year_days - order + 1
+  for (Day in 1:last_day) {
+    end_day <- Day + order - 1
     means_order[Day] <- mean(flows[flows$Year ==
-                                    Year, ]$Media[Day:End_day])
+                                    year, ]$Media[Day:end_day])
   }
   min_value <- min(means_order)
   return(min_value)
 }
 
-Qb_Year <- function(min_values) {
+Qb_year <- function(min_values) {
   ratio <- NULL
-  for (order in 1:(Max_order - 1)) {
+  for (order in 1:(max_order - 1)) {
     if (min_values[order] != 0) {
       ratio[order] <- (min_values[order + 1] -
-                        min_values[order])/min_values[order]
+                        min_values[order]) / min_values[order]
     }
   }
-  Qb_Year <- min_values[which((ratio == max(ratio[!is.na(ratio)]))) +
+  Qb_year <- min_values[which((ratio == max(ratio[!is.na(ratio)]))) +
                          1]
-  return(Qb_Year)
+  return(Qb_year)
 }
 
-Qb <- function(flows, Year_init, Year_final) {
-  Qb_Years <- NULL
-  for (Year in Year_init:Year_final) {
+Qb <- function(flows, year_init, year_final) {
+  Qb_years <- NULL
+  for (Year in year_init:year_final) {
     min_values <- NULL
     year_days <- sum(flows$Year == Year)
-    for (order in 1:Max_order) {
+    for (order in 1:max_order) {
       min_values[order] <- min_value(Year, order,
                                     year_days, flows)
     }
-    Qb_Years[Year - (Year_init) + 1] <- Qb_Year(min_values)
+    Qb_years[Year - (year_init) + 1] <- Qb_year(min_values)
   }
-  
-  Qb <- mean(Qb_Years)
+  Qb <- mean(Qb_years)
   return(Qb)
 }
 
-Qb_value <- Qb(flows, Year_init, Year_final)
+Qb_value <- Qb(flows, year_init, year_final)
 print(paste("Base flow is ", Qb_value, "m3/s"))
